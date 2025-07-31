6c57cffb6a2aaf2223c3c30d0b956d309791490f
Manda Tiavina <hei.manda.16@gmail.com>
2025-07-31 09:45:59 +0300

    The final code

--- main.py
+++ main.py
@@ -1,73 +1,63 @@
-import json
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
-from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
-
-class EventModel(BaseModel):
-    name: str
-    description: str
-    start_date: str
-    end_date: str
-
-
-events_store: List[EventModel] = []
+# Model for Player
+class posts(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: str
 
 
-def serialized_stored_events():
-    events_converted = []
-    for event in events_store:
-        events_converted.append(event.model_dump())
-    return events_converted
+# In-memory store for players
+published: List[posts] = []
 
+# Serialize players to JSON-compatible format
+def serialized_posts():
+    posts_converted = []
+    for posts in published:
+        posts_converted.append(posts.model_dump())
+    return posts_converted
 
-@app.get("/")
-def root(request: Request):
-    accept_headers = request.headers.get("Accept")
-    authorization = request.headers.get("x-api-key")
-    if accept_headers != "text/html" and accept_headers != "text/plain":
-        return JSONResponse(content={"message": f"Media Type not supported : {accept_headers}"}, status_code=400)
-    if authorization != "12345678":
-        return JSONResponse(content={"message": f"Provided key unknown : {authorization}"}, status_code=403)
-    with open("welcome.html", "r", encoding="utf-8") as file:
+@app.get("/ping")
+def hello():
+        return Response(content="pong", status_code=200)
+
+@app.get("/home")
+def welcome():
+    with open("home.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
 
+@app.post("/posts")
+def new_posts(posts_published: List[posts]):
+    published.extend(posts_published)
+    return Response(content={"posts": serialized_posts()}, status_code=201)
 
-@app.get("/events")
-def list_events():
-    return {"events": serialized_stored_events()}
+@app.get("/posts")
+def list_posts():
+    return JSONResponse(content={"players": serialized_posts()}, status_code=200)
 
-
-@app.post("/events")
-def new_events(event_payload: List[EventModel]):
-    events_store.extend(event_payload)
-    return {"events": serialized_stored_events()}
-
-
-@app.put("/events")
-def update_or_create_events(event_payload: List[EventModel]):
-    global events_store  # `global` to indicate here that we want to use global variable not creating local events_store var
-
-    for new_event in event_payload:
-        # Used to check if event already exists later
+@app.put("/posts")
+def update_or_create_posts(posts_published: List[posts]):
+    global published
+    for new_posts in posts_published:
         found = False
-        for i, existing_event in enumerate(events_store):
-            if new_event.name == existing_event.name:
-                events_store[i] = new_event
+        for i, existing_posts in enumerate(published):
+            if new_posts.title == existing_posts.title:
+                published[i] = new_posts
                 found = True
                 break
         if not found:
-            events_store.append(new_event)
-    return {"events": serialized_stored_events()}
-
+            published.append(new_posts)
+    return JSONResponse(content={"posts": serialized_posts()}, status_code=200)
 
 @app.get("/{full_path:path}")
 def catch_all(full_path: str):
-    with open("not_found.html", "r", encoding="utf-8") as file:
-        html_content = file.read()
-    return Response(content=html_content, status_code=200, media_type="text/html")
+        with open("not_found.html", "r", encoding="utf-8") as file:
+            html_content = file.read()
+        return Response(content=html_content, status_code=404, media_type="text/html")
